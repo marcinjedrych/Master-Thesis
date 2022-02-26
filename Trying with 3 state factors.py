ea5ee3bd61c46cd90cab80276d9097396220a298
marcinjedrych <96244063+marcinjedrych@users.noreply.github.com>
2022-02-26 13:47:42 +0100

    Update Trying with 3 state factors.py

--- Trying with 3 state factors.py
+++ Trying with 3 state factors.py
@@ -2,7 +2,7 @@
 """
 A simulation of the experiment with 3 state factors. (only with 2 decks)
 
-Created on Tue Feb 17 12:11:06 2022
+Created on Tue Feb 26 13:18:06 2022
 @author: Marcin Jedrych
 """
 
@@ -28,11 +28,11 @@ choice_action_names = ['Start', 'ChD1', 'ChD2']
 """ Define `num_controls` below """
 num_controls = [len(context_action_names), len(choice_action_names)]
 
-behavior_obs_names = ['Null', 'High', 'Low']
+reward_obs_names = ['Null', 'High', 'Low']
 choice_obs_names = ['Start', 'ChD1', 'ChD2']
 
 """ Define `num_obs` and `num_modalities` below """
-num_obs = [len(behavior_obs_names), len(choice_obs_names)]
+num_obs = [len(reward_obs_names), len(choice_obs_names)]
 num_modalities = len(num_obs)
 
 
@@ -41,13 +41,16 @@ num_modalities = len(num_obs)
 
 A = utils.obj_array( num_modalities )
 
-prob_win = [0.5,0.5] # what is the probability of win for each deck
+prob_win = [0.5,0.5] # what is the probability of win for each deck (volgens het generatieve process)
 
-pH1 = 0.55
-pH2 = 0.6
+#kansen volgens het geenertieve model
+pH1_G = 0.55 #kans om high reward te zien als deck 1 goed is
+pH1_B = 0.6  #kans om high reward te zien als deck 1 slecht is
+pH2_G = 0.55  #kans om high reward te zien als deck 2 goed is
+pH2_B = 0.6  #kans om high reward te zien als deck 2 slecht is
 
 # 3x2x2x3 = 36 cells
-A_behavior = np.zeros((len(behavior_obs_names), len(D1_names), len(D2_names), len(choice_names)))
+A_reward = np.zeros((len(reward_obs_names), len(D1_names), len(D2_names), len(choice_names)))
 
 # 3x2x2x3 = 36 cells
 A_choice = np.zeros((len(choice_obs_names), len(D1_names), len(D2_names), len(choice_names)))
@@ -56,20 +59,19 @@ for choice_id, choice_name in enumerate(choice_names):
     
     if choice_name == 'Start':
 
-        A_behavior[0,:,:,choice_id] = 1.0
+        A_reward[0,:,:,choice_id] = 1.0
 
     elif choice_name == 'ChD1':
         
-        A_behavior[choice_id,:,:] = np.array([[0,pH1,1-pH1],[0,1-pH1,pH1]])
+        for loop in range(2):
+            A_reward[1:,:,loop, choice_id] = np.array([[pH1_G,pH1_B],[1-pH1_G,1-pH1_B]])
     
     elif choice_name == 'ChD2':
         
-        A_behavior[choice_id,:,:] = np.array([[0,pH2,1-pH2],[0,1-pH2,pH2]])
-        
-#dit is waarschijnlijk verkeerd, nog bekijken.^
+        for loop in range(2):
+            A_reward[1:,loop,:, choice_id] = np.array([[pH2_G,pH2_B],[1-pH2_G,1-pH2_B]])
 
-  
-A[0] = utils.norm_dist(A_behavior)
+A[0] = utils.norm_dist(A_reward)
 
 A_choice = np.zeros((len(choice_obs_names), len(D1_names), len(D2_names), len(choice_names)))
 
@@ -124,15 +126,15 @@ D[1] = D_context2
 
 D_choice = np.zeros(len(choice_names))
 
-D_choice[choice_names.index("Start")] = 1.0
+D_choice[choice_names.index("Start")] = 1.0 #agent in start
 
 D[2] = D_choice
 
 ############################################
 
-my_agent = Agent(A = A, B = B, C = C, D = D)
+my_agent = Agent(A = A, B = B, C = C, D = D)  # = generative model
 
-class omgeving(object):
+class omgeving(object):      # = generative process
 
   def __init__(self, context = None, p_consist = 0.8):
 
@@ -145,73 +147,76 @@ class omgeving(object):
 
     self.p_consist = p_consist
 
-    self.behavior_obs_names = ['Null', 'High', 'Low']
+    self.reward_obs_names = ['Null', 'High', 'Low']
 
 
   def step(self, action):
 
     if action == "Start":
-      observed_behavior = "Null"
+      observed_reward = "Null"
       observed_choice   = "Start"
 
     elif action == "ChD1":
       observed_choice = "ChD1"
       if self.context == "High":
-        observed_behavior = self.behavior_obs_names[utils.sample(np.array([0.0, self.p_consist, 1 - self.p_consist]))]
+        observed_reward = self.reward_obs_names[utils.sample(np.array([0.0, self.p_consist, 1 - self.p_consist]))]
       else:
-        observed_behavior = self.behavior_obs_names[utils.sample(np.array([0.0, 1 - self.p_consist, self.p_consist]))]
+        observed_reward = self.reward_obs_names[utils.sample(np.array([0.0, 1 - self.p_consist, self.p_consist]))]
     
     elif action == "ChD2":
       observed_choice = "ChD2"
       if self.context == "High":
-        observed_behavior = self.behavior_obs_names[utils.sample(np.array([0.0, self.p_consist, 1 - self.p_consist]))]
+        observed_reward = self.reward_obs_names[utils.sample(np.array([0.0, self.p_consist, 1 - self.p_consist]))]
       else:
-        observed_behavior = self.behavior_obs_names[utils.sample(np.array([0.0, 1 - self.p_consist, self.p_consist]))]
+        observed_reward = self.reward_obs_names[utils.sample(np.array([0.0, 1 - self.p_consist, self.p_consist]))]
     
-    obs = [observed_behavior, observed_choice]
+    obs = [observed_reward, observed_choice]
 
     return obs
+
+#print(my_agent.infer_policies()) 
 
 def run_active_inference_loop(my_agent, my_env, T = 5):
 
   """ Initialize the first observation """
-  obs_label = ["Null", "Start"]  # agent observes a `Null` behavior, and seeing itself in the `Start` location
-  obs = [behavior_obs_names.index(obs_label[0]), choice_obs_names.index(obs_label[1])]
+  obs_label = ["Null", "Start"]  # agent observes a `Null` reward, and seeing itself in the `Start` location
+  obs = [reward_obs_names.index(obs_label[0]), choice_obs_names.index(obs_label[1])]
+  print('Initial observation:',obs)
+  
   ent = np.zeros((T, num_factors))
   
   for t in range(T):
-    qs = my_agent.infer_states(obs)
-    print("***", qs[0], qs[1])
-    #print("QS", qs)
+    qs = my_agent.infer_states(obs)  # agent changes beliefs about states based on observation
+    print("Beliefs about the decks reward: Deck1 =", qs[0],"Deck2 =", qs[1])
 
-    q_pi, efe = my_agent.infer_policies()
+    q_pi, efe = my_agent.infer_policies() #based on beliefs agent gives value to actions
+    print('EFE for each action:', efe)  #[Start, ChD1, ChD2]
     
     ##forced choice trials
     if t < 6:
         choice_action = 'ChD1'
     else:
-        chosen_action_id = my_agent.sample_action()
-        #print('chosen action id',chosen_action_id )
+        chosen_action_id = my_agent.sample_action()   #agent choses action with less negative expected free energy
+        #print('chosen action id',chosen_action_id)
     
         movement_id = int(chosen_action_id[2])
-        #print("movement id", movement_id , '\n')
-        
+        #print("movement id", movement_id)
+         
         choice_action = choice_action_names[movement_id]
-        #print("choice action", choice_action, '\n')
+        print("Chosen action:", choice_action)
 
     obs_label = my_env.step(choice_action)
-    #print("obslabel", obs_label, '\n')
+    #print("obslabel", obs_label)
 
-    obs = [behavior_obs_names.index(obs_label[0]), choice_obs_names.index(obs_label[1])]
-    #print("obs", obs, '\n')
+    obs = [reward_obs_names.index(obs_label[0]), choice_obs_names.index(obs_label[1])]
 
     print(f'Action at time {t}: {choice_action}')
-    print(f'Behavior at time {t}: {obs_label[0]}' +'\n')
-     
+    print(f'Reward at time {t}: {obs_label[0]}')
+    print(f'New observation:',choice_action,'&', reward_obs_names[obs[0]] + ' reward', '\n')
+    
   return ent
 
-p_consist = 0.8 # This is how consistent behavior is with actual character
+p_consist = 0.8 # This is how consistent reward is with actual
 env = omgeving(p_consist = p_consist)
 T = 20
 entr = run_active_inference_loop(my_agent, env, T = T)
-
